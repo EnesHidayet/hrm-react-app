9b92b95d6fd73cc2cb491f099235f75d692c71b6
cem327 <cmylmz327@hotmail.com>
2024-05-06 12:39:03 +0300

    Company Page added. An example data is set to preview.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import User from './pages/User';
-import PersonelYonetim from './pages/PersonelYonetim';
-import AddNewEmployee from './pages/AddNewEmployee';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import PersonelYonetim from "./pages/PersonelYonetim";
+import AddNewEmployee from "./pages/AddNewEmployee";
+import Company from "./pages/Company";
 
 function App() {
-  const [token, setToken] = useState(''); // Use state to manage token
+  const [token, setToken] = useState(""); // Use state to manage token
 
   useEffect(() => {
-    const storedToken = sessionStorage.getItem('token');
+    const storedToken = sessionStorage.getItem("token");
     if (storedToken) {
       setToken(storedToken);
     }
@@ -20,19 +21,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login setToken={setToken} />} /> // Pass setToken to Login
-        <Route path='/register' element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setToken={setToken} />} /> // Pass
+        setToken to Login
+        <Route path="/register" element={<Register />} />
         <Route
-          path='/user'
-          element={token ? <User /> : <Navigate to='/login' replace />}
+          path="/user"
+          element={token ? <User /> : <Navigate to="/login" replace />}
         />
-        <Route path='/personel-yonetim' element={<PersonelYonetim />} />
-        <Route path='/add-new-employee' element={<AddNewEmployee />} />
+        <Route path="/personel-yonetim" element={<PersonelYonetim />} />
+        <Route path="/add-new-employee" element={<AddNewEmployee />} />
+        <Route path="/company" element={<Company />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
