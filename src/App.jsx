78b2feb7fa68c5c4272eb432fa89548355137af2
denizmustafabektas <denizmustafabektas@gmail.com>
2024-05-06 15:51:51 +0300

    PermissionPage page and OtherPermission component has been created

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,8 @@ import Home from './pages/Home';
 import User from './pages/User';
 import PersonelYonetim from './pages/PersonelYonetim';
 import AddNewEmployee from './pages/AddNewEmployee';
-<<<<<<< HEAD
 import PermissionPage from './pages/PermissionPage';
 import OtherPermissionData from './components/molekul/OtherPermissionData';
-=======
-import AktifPersonelListesi from './components/molekul/AktifPersonelListesi';
-import InaktifPersonelListesi from './components/molekul/InaktifPersonelListesi';
->>>>>>> origin/master
 
 function App() {
   const [token, setToken] = useState(""); // Use state to manage token
@@ -28,31 +23,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login setToken={setToken} />} /> 
         <Route path='/register' element={<Register />} />
-=======
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login setToken={setToken} />} /> // Pass
-        setToken to Login
-        <Route path="/register" element={<Register />} />
->>>>>>> origin/master
         <Route
           path="/user"
           element={token ? <User /> : <Navigate to="/login" replace />}
         />
         <Route path='/personel-yonetim' element={<PersonelYonetim />} />
         <Route path='/add-new-employee' element={<AddNewEmployee />} />
-<<<<<<< HEAD
         <Route path='/permission-page' element={<PermissionPage />} />
         <Route path='/permission-page' element={<OtherPermissionData />} />
-=======
-        <Route path='/active' element={<AktifPersonelListesi />} />
-        <Route path='/inactive' element={<InaktifPersonelListesi />} />
-
-
->>>>>>> origin/master
       </Routes>
     </BrowserRouter>
   );
