9f43cc1f7a1bf42d3e6dbdc6a2d4fb9b297003f2
cem327 <cmylmz327@hotmail.com>
2024-05-13 11:43:54 +0300

    Company Carts Page. Backend integration of Companies. Paths added to App. Types and slices added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 // import React, { useEffect, useState } from 'react';
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import User from './pages/User';
-import PersonelYonetim from './pages/PersonelYonetim';
-import AddNewEmployee from './pages/AddNewEmployee';
-import DefinePermission from './pages/DefinePermission';
-import ForgotPassword from './pages/ForgotPassword';
-import UpdatePassword from './pages/UpdatePassword';
-import AcceptPermissions from './pages/AcceptPermissions';
-import UserDemo from './pages/UserDemo';
-import UserInformationUpdate from './pages/UserInformationUpdate';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import PersonelYonetim from "./pages/PersonelYonetim";
+import AddNewEmployee from "./pages/AddNewEmployee";
+import DefinePermission from "./pages/DefinePermission";
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import AcceptPermissions from "./pages/AcceptPermissions";
+import UserDemo from "./pages/UserDemo";
+import UserInformationUpdate from "./pages/UserInformationUpdate";
+import Company from "./pages/Company";
 
 function App() {
   const [token, setToken] = useState(""); // Use state to manage token
@@ -27,21 +28,25 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login setToken={setToken} />} /> 
-        <Route path='/register' element={<Register />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/update-password' element={<UpdatePassword />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/update-password" element={<UpdatePassword />} />
         <Route
           path="/user"
           element={token ? <User /> : <Navigate to="/login" replace />}
         />
-        <Route path='/personel-yonetim' element={<PersonelYonetim />} />
-        <Route path='/add-new-employee' element={<AddNewEmployee />} />
-        <Route path='/define-permission' element={<DefinePermission />} />
-        <Route path='/accept-permissions' element={<AcceptPermissions />} />
-        <Route path='/user-demo' element={<UserDemo/>} />
-        <Route path='/user-information-update' element={<UserInformationUpdate/>} />
+        <Route path="/personel-yonetim" element={<PersonelYonetim />} />
+        <Route path="/add-new-employee" element={<AddNewEmployee />} />
+        <Route path="/define-permission" element={<DefinePermission />} />
+        <Route path="/accept-permissions" element={<AcceptPermissions />} />
+        <Route path="/user-demo" element={<UserDemo />} />
+        <Route
+          path="/user-information-update"
+          element={<UserInformationUpdate />}
+        />
+        <Route path="/company" element={<Company />} />
       </Routes>
     </BrowserRouter>
   );
